20ea9e2f68688df22103dab6095569b031583941
entermedia-community <entermedia-community@entermediasoftware.com>
2014-07-23 14:04:12 -0400

    some new ajax main stuff

--- webapp/emsite/components/annotations/ajax/main.js
+++ webapp/emsite/components/annotations/ajax/main.js
@@ -5,7 +5,7 @@ app = jQuery("#application");
 home = app.data("home");
 
 em = {
-  unit: {}
+	unit: {}
 };
 
 apphome = home + app.data("apphome");
@@ -16,26 +16,66 @@ collectionid = $("#collectiontoplevel").data("collectionid");
 
 catalogid = 'emsite/catalog';
 
-if (window.WebSocket) {
-  base_destination = "ws://localhost:8080/entermedia/services/websocket/echoProgrammatic";
-  final_destination = "" + base_destination + "?catalogid=" + catalogid + "&collectionid=" + collectionid;
-  connection = new WebSocket(final_destination);
-  connection.onopen = function(e) {
-    console.log('Opened a connection!');
-    console.log(e);
-    return em.unit;
-  };
-  connection.onclose = function(e) {
-    console.log('Closed a connection!');
-    console.log(e);
-    return em.unit;
-  };
-  connection.onerror = function(e) {
-    console.log('Connection error!');
-    console.log(e);
-    return em.unit;
-  };
-  $scope.connection = connection;
+$scope.add = function(name, model) {
+	$scope[name] = model;
 }
 
+//socket initialization
+if (window.WebSocket) {
+	socket = WebSocket;
+} else if (window.MozWebSocket) {
+	socket = MozWebSocket;
+} else {
+	"We're screwed";
+	socket = null;
+}
+
+if (socket)
+	base_destination = "ws://localhost:8080/entermedia/services/websocket/echoProgrammatic";
+	final_destination = "" + base_destination + "?catalogid=" + catalogid + "&collectionid=" + collectionid;
+	connection = new socket(final_destination);
+	connection.onopen = function(e) {
+		console.log('Opened a connection!');
+		console.log(e);
+		return em.unit;
+	};
+	connection.onclose = function(e) {
+		console.log('Closed a connection!');
+		console.log(e);
+		return em.unit;
+	};
+	connection.onerror = function(e) {
+		console.log('Connection error!');
+		console.log(e);
+		return em.unit;
+	};
+	$scope.add('connection', connection);
+}
+
+//load user data
+$.getJSON '/entermedia/services/json/users/status.json', function(data) {
+	console.log('from user auth:', data);
+	$scope.add('currentUser', data);
+	return em.unit;
+}
+
+// load asset data
+$.ajax({
+	type: "GET",
+	url: "" + apphome + "/components/annotations/json/viewassets.json?id=" + collectionid,
+	async: false,
+	error: function(data, status, err) {
+		console.log('from error:', data);
+		return em.unit;
+	},
+	success: function(data) {
+		console.log('from success:', data);
+		$scope.add('assets', data);
+		return em.unit;
+	},
+	failure: function(errMsg) {
+		alert(errMsg);
+		return em.unit;
+	}
+});
 
