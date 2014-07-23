2b63683620a2582b822d1606dbadc2feeac3b392
Christopher Burkey <cburkey@openedit.org>
2014-07-23 17:19:06 -0400

    Jangular is born

--- webapp/emsite/components/annotations/ajax/jAngular.js
+++ webapp/emsite/components/annotations/ajax/jAngular.js
@@ -1,7 +1,39 @@
 // jAngular.js
+
+var Scope = function() {
+	var parentScope;
+	var out = {
+		add: function(name, model) {
+			this[name] = model;
+		},
+		get: function(name) 
+		{
+			var command = name;
+			if( !name.indexOf("if") == 0 )  //starts with
+			{
+				command = "this." + name;
+			}
+			var found = eval(command);
+			if( parentScope != null && found == null )
+			{
+				return parentScope.get(name);
+			}
+			return found;
+			//return $scope[name];
+		},
+		createScope: function()
+		{
+			var newscope = new Scope();
+			newscope.parentScope  = this;
+			return newscope
+		}
+		}
+		return out;
+	}
+
+
 var Replacer = function() {
 	var out = {
-
 		replace : function( inCode, scope)
 		{
 			if( inCode == null)
@@ -18,15 +50,14 @@ var Replacer = function() {
 				}
 				
 				var key = inCode.substring(start+2,end);
-				var variable = scope.get(value); //check for property
+				var variable = scope.get(key); //check for property
 				
-				if( variable != null)
+				if( typeof variable != 'undefined')
 				{
-					String sub = variable.toString();
 					//sub = replace(sub,inValues);  //recursive
-					inCode = inCode.substring(0,start) + sub + inCode.substring(end+1);
-					start = start + sub.length();
-				}
+					inCode = inCode.substring(0,start) + variable + inCode.substring(end+2);
+					start = start + variable.length + 1;
+				}				
 				else
 				{
 					start = end; //could not find a hit, go to the next one
@@ -34,30 +65,44 @@ var Replacer = function() {
 			}
 			return inCode;
 		}	
-	}
+	};
 	return out;
-})
+};
 
-var Scope = function() {
-	var parentScope;
-	var out = {
-		add: function(name, model) {
-			$scope[name] = model;
-		},
-		get: function(name) 
+var jAngular =  {};
+
+jAngular.init = function(scope)
+{ 
+	var replacer = new Replacer();
+	
+	//Live query this?
+	$( 'li[ng-repeat]' ).each(function( index ) 
+	{
+		var li = $(this);
+		var vars = li.attr("ng-repeat");
+		var split = vars.indexOf(" in ");
+		
+		var rowname = vars.substring(0,split);
+		var loopname = vars.substring(split + 4,vars.length );
+		
+		var rows = scope.get(loopname);  //TODO: Find the name
+		
+		//set a local scope of asset = rows[i];
+		var content = li.html();
+		li.html("");
+		if( rows )
 		{
-			var found = eval("this." + name);
-			if( parentScope != null && found == null )
-			{
-				return parentScope.get(name);
-			}
-			return found;
-			//return $scope[name];
-		},
-		createScope: function(parent)
-		{
-			parentScope = parent;
-		}
-		}
-		return out;
-	}
+			$.each(rows, function(index, value) {
+				//TODO: replace scope variables
+				var localscope = scope.createScope();
+				localscope.add(rowname,value);
+				var evalcontent = replacer.replace(content,localscope);
+				evalcontent = evalcontent.replace("ng-src","src");
+				li.append(evalcontent);
+				
+	        });
+	     }
+		
+	});
+	
+};
--- webapp/emsite/components/annotations/ajax/main.js
+++ webapp/emsite/components/annotations/ajax/main.js
@@ -1,22 +1,57 @@
 //jAnqular controller
-em = { //Old Junk
-	unit: {}
-};
-
-var $scope = new Scope();
-
-	
 
 jQuery(document).ready(function() 
 { 
-
-	$scope.add("app", jQuery("#application") );
-	$scope.add("home" ,$scope.app.data("home") );
-	$scope.add("apphome" , $scope.app.data("apphome") );
-	$scope.add("componentroot" ,$scope.app.data("home") );
-	$scope.add("collectionid", $("#collectiontoplevel").data("collectionid") );
-	$scope.add("catalogid" ,'emsite/catalog');
+	var scope = new Scope();
+	scope.add("app", jQuery("#application") );
+	scope.add("home" ,scope.app.data("home") );
+	scope.add("apphome" , scope.app.data("apphome") );
+	scope.add("componentroot" ,scope.app.data("home") );
+	scope.add("collectionid", $("#collectiontoplevel").data("collectionid") );
+	scope.add("catalogid" ,'emsite/catalog');
 	
+	//$.getScript(scope.apphome + "/components/annotations/ajax/AnnotationModels.js");	
+	loadModels(scope);
+	
+	loadSelectors(scope);
+	
+	jAngular.init(scope);
+	
+});	
+
+loadSelectors = function(scope)
+{
+	//TODO: Search for all ng-click?
+	$("div.annotations-carousel a img").livequery('click', function() 
+	{
+		var theimg = jQuery(this);
+		var code = theimg.attr("ng-click");
+		eval(code);
+	});
+
+}
+
+loadModels = function(scope)
+{
+
+	// load asset data
+	jQuery.ajax({
+		type: "GET",
+		url: "" + scope.apphome + "/components/annotations/json/viewassets.json?id=" + scope.collectionid,
+		async: false,
+		error: function(data, status, err) {
+			console.log('from error:', data);
+		},
+		success: function(data) {
+			console.log('from success:', data);
+			scope.add('assets', data);
+		},
+		failure: function(errMsg) {
+			alert(errMsg);
+		}
+	});
+
+
 	//socket initialization
 	if (window.WebSocket) {
 		socket = WebSocket;
@@ -30,24 +65,21 @@ jQuery(document).ready(function()
 	if (socket)
 	{
 		base_destination = "ws://localhost:8080/entermedia/services/websocket/echoProgrammatic";
-		final_destination = "" + base_destination + "?catalogid=" + $scope.catalogid + "&collectionid=" + $scope.collectionid;
+		final_destination = "" + base_destination + "?catalogid=" + scope.catalogid + "&collectionid=" + scope.collectionid;
 		connection = new socket(final_destination);
 		connection.onopen = function(e) {
 			console.log('Opened a connection!');
 			console.log(e);
-			return em.unit;
 		};
 		connection.onclose = function(e) {
 			console.log('Closed a connection!');
 			console.log(e);
-			return em.unit;
 		};
 		connection.onerror = function(e) {
 			console.log('Connection error!');
 			console.log(e);
-			return em.unit;
 		};
-		$scope.add('connection', connection);
+		scope.add('connection', connection);
 	}
 	*/
 	
@@ -55,54 +87,51 @@ jQuery(document).ready(function()
 	/*
 	$.getJSON( '/entermedia/services/json/users/status.json', function(data) {
 		console.log('from user auth:', data);
-		$scope.add('currentUser', data);
-		return em.unit;
+		scope.add('currentUser', 	jAngular.init(scope);
+		data);
 	});
 	*/
 	
-	// load asset data
-	jQuery.ajax({
-		type: "GET",
-		url: "" + $scope.apphome + "/components/annotations/json/viewassets.json?id=" + $scope.collectionid,
-		async: false,
-		error: function(data, status, err) {
-			console.log('from error:', data);
-			return em.unit;
-		},
-		success: function(data) {
-			console.log('from success:', data);
-			$scope.add('assets', data);
-			return em.unit;
-		},
-		failure: function(errMsg) {
-			alert(errMsg);
-			return em.unit;
-		}
-	});
+var $fabric = {
+	init: function(path) {
+		var returnCanvas;
+		returnCanvas = {};
+		(function() {
+			var canvas, docGet;
+			docGet = function(id) {
+				return document.getElementById(id);
+			};
+			canvas = this.__canvas = new fabric.Canvas('annotation_canvas');
+			canvas.on("after:render", function() {
+			});
+			fabric.util.loadImage(path, function(src) {
+				var center, realImage;
+				realImage = new fabric.Image(src);
+				canvas.setWidth(realImage.width);
+				canvas.setHeight(realImage.height);
+				center = canvas.getCenter();
+				canvas.setBackgroundImage(realImage, canvas.renderAll.bind(canvas));
+			});
+			returnCanvas = canvas;
+		})();
+		(function() {
+			return fabric.util.addListener(fabric.window, 'load', function() {
+				var canvas, canvases, _i, _ref;
+				canvas = this.__canvas || this.canvas;
+				canvases = this.__canvases || this.canvases;
+				canvas && canvas.calcOffset && canvas.calcOffset();
+				if (canvases && canvases.length) {
+					for (_i = 0, _ref = canvases.length; 0 <= _ref ? _i <= _ref : _i >= _ref; 0 <= _ref ? _i++ : _i--) {
+						canvases[i].calcOffset();
+					}
+				}
+			});
+		})();
+		return {
+			canvas: returnCanvas
+		};
+	}
+};
+scope.add('fabric', $fabric);
 
-	$.getScript($scope.apphome + "/components/annotations/ajax/FabricModel.js");
-	$.getScript($scope.apphome + "/components/annotations/ajax/Replacer.js");
-	
-	var replacer = new Replacer();
-	
-	$( 'li[ng-repeat]' ).each(function( index ) 
-	{
-		var li = $(this);
-		var vars = li.attr("ng-repeat");
-		
-		var rows = $scope.get("assets");  //TODO: Find the name
-		
-		//set a local scope of asset = rows[i];
-		var content = li.html();
-		li.html("");
-		$.each(rows, function(index, value) {
-			//TODO: replace scope variables
-			var evalcontent = replacer.replace(content,$scope);
-			
-			li.append(content);
-        });
-		
-	});
-	
-	
-});	
+}
